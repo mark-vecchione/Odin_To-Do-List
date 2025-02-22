1059fb0420af8f6e87a5aea43ebc9b960e2e78de
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-02-21 19:33:56 -0500

    created baseline html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import { add } from 'date-fns';
 import {toDoItem, createToDoItem, updateToDoItem} from './toDoItem'
 
 const testOne = createToDoItem("test note 2", "this is a test note", "9/7/24","low");
 
-updateToDoItem(testOne, {title: "New", priority: "Hgh"});
-console.log("after update:", testOne);
+// add event listener to new task button//
+
+const addTaskButton = document.querySelector('#addTaskButton');
+
+addTaskButton.addEventListener('click',(event) => {
+    //prevent refresh//
+    event.preventDefault();
+
+    //pop-up task form//
+
+    //submit task//
+
+    //create new to do item//
+})
