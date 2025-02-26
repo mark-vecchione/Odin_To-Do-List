d686572604a09450b7a778f05150e805068d2e78
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-02-25 20:53:46 -0500

    Create task and project form submission functions

--- src/forms.js
+++ src/forms.js
@@ -1,9 +1,100 @@
+// global const to grab task and project form wrappers//
+const taskFormWrapper = document.getElementById('taskFormWrapper');
+const projectFormWrapper = document.getElementById('projectFormWrapper');
+
+// display task form and project form//
 export function showTaskForm() {
-    const taskFormWrapper = document.getElementById('taskFormWrapper');
-    taskFormWrapper.style.display = 'flex';
+  taskFormWrapper.style.display = 'flex';
 }
 
 export function showProjectForm() {
-    const projectFormWrapper = document.getElementById('projectFormWrapper');
     projectFormWrapper.style.display = 'flex';
+  }
+
+// hide task form and project form
+export function hideTaskForm() {
+  taskFormWrapper.style.display = 'none';
 }
+
+export function hideProjectForm() {
+    projectFormWrapper.style.display = 'none';
+}
+
+
+// add event listener to new task button, display task form//
+
+export function openTaskForm() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const addTaskButton = document.querySelector('.addTaskButton');
+        if(addTaskButton) {
+            addTaskButton.addEventListener('click', (event) => {
+                //prevent refresh//
+                event.preventDefault();
+    
+                // display task form//
+                showTaskForm();
+            })
+        }
+    })
+};
+
+
+// add event listener to new project button, display project form//
+export function openProjectForm() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const addProjectButton = document.querySelector('.addProjectButton');
+        if(addProjectButton) {
+            addProjectButton.addEventListener('click', (event) => {
+                //prevent refresh//
+                event.preventDefault();
+
+                // display task form//
+                showProjectForm();
+            })
+        }
+    })
+}
+
+//attach event listener to task and project form//
+const taskForm = document.querySelector('#taskForm');
+const projectForm = document.querySelector('#projectForm');
+
+// task form submission //
+
+export function submitTaskForm() {
+    taskForm.addEventListener('submit', (event) => {
+        //prevent refresh//
+        event.preventDefault();
+
+        // create form data object //
+        const taskFormData = new FormData(taskForm);
+        const taskData = Object.fromEntries(taskFormData);
+
+        //hide form//
+        hideTaskForm();
+
+        //log data to test//
+
+        console.log(taskData);
+    });
+}
+
+//project form submission//
+
+export function submitProjectForm() {
+    projectForm.addEventListener('submit', (event) => {
+        //prevent refresh//
+        event.preventDefault();
+
+        // create form data object //
+        const projectFormData = new FormData(projectForm);
+        const projectData = Object.fromEntries(projectFormData);
+        //hide form//
+        hideTaskForm();
+
+        //log data to test//
+        console.log(projectData);
+    });
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
 import "./styles.css";
 import { add } from 'date-fns';
 import {toDoItem, createToDoItem, updateToDoItem} from './toDoItem';
-import {showTaskForm, showProjectForm} from './forms'
+import {openTaskForm, openProjectForm, hideTaskForm, hideProjectForm, submitTaskForm, submitProjectForm} from './forms'
 
 
 const testOne = createToDoItem("test note 2", "this is a test note", "9/7/24","low");
 
-// add event listener to new task button//
+openTaskForm();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const addTaskButton = document.querySelector('.addTaskButton');
-    if(addTaskButton) {
-        addTaskButton.addEventListener('click', (event) => {
-            //prevent refresh//
-            event.preventDefault();
+openProjectForm();
 
-            // display task form//
-            showTaskForm();
-        })
-    }
-});
+submitTaskForm();
 
-// add event listener to new project button//
-
-document.addEventListener('DOMContentLoaded', () => {
-    const addProjectButton = document.querySelector('.addProjectButton');
-    if(addProjectButton) {
-        addProjectButton.addEventListener('click', (event) => {
-            //prevent refresh//
-            event.preventDefault();
-
-            // display task form//
-            showProjectForm();
-        })
-    }
-});
+submitProjectForm();
